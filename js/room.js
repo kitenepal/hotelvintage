5f606b84bc41d7a53ab8da503bd2a3a0bff30bfe
Pirates357 <hacking.pirates@gmail.com>
2018-02-05 18:14:22 +0545

    responsive javascript for tab in rooms section

--- js/room.js
+++ js/room.js
@@ -1,6 +1,18 @@
-    if(screen.width<=590){
-      document.getElementById("room-name").innerHTML = "DDR";
-      document.getElementById("room-name-two").innerHTML = "STR";
-      document.getElementById("room-name-three").innerHTML = "DRB";
-      document.getElementById("room-name-four").innerHTML = "TRB";
-    }
+// if($(window).width()<=590){
+// 	document.getElementById("room-name").innerHTML = "DDR";
+// 	document.getElementById("room-name-two").innerHTML = "STR";
+// 	document.getElementById("room-name-three").innerHTML = "DRB";
+// 	document.getElementById("room-name-four").innerHTML = "TRB";
+// }
+
+
+
+window.onresize = function(event) {  
+    if ($(window).width()<= 590){ 
+       	document.getElementById("room-name").innerHTML = "DDR";
+				document.getElementById("room-name-two").innerHTML = "STR";
+				document.getElementById("room-name-three").innerHTML = "DRB";
+				document.getElementById("room-name-four").innerHTML = "TRB";
+    }  
+ }
+
