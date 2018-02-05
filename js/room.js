84330ff39407c1d7ed980ac91472b3c8334a8182
NidanStha <nidanshrestha@gmail.com>
2018-02-05 17:41:49 +0545

    room final responsive

---
+++ js/room.js
@@ -0,0 +1,6 @@
+    if(screen.width<=590){
+      document.getElementById("room-name").innerHTML = "DDR";
+      document.getElementById("room-name-two").innerHTML = "STR";
+      document.getElementById("room-name-three").innerHTML = "DRB";
+      document.getElementById("room-name-four").innerHTML = "TRB";
+    }
